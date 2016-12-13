79388d13486a8e1174acc1c7da616a90d32fdf34
kokichi88 <kokichi88@gmail.com>
2016-12-13 14:02:12 +0700

    add GPU checking for detecting more accurately

--- EmulatorDetectorProject/EmulatorDetector/src/main/java/net/skoumal/emulatordetector/EmulatorDetector.java
+++ EmulatorDetectorProject/EmulatorDetector/src/main/java/net/skoumal/emulatordetector/EmulatorDetector.java
@@ -2,6 +2,9 @@ package net.skoumal.emulatordetector;
 
 import android.os.Build;
 import android.util.Log;
+import java.io.File;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Environment;
 
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -26,56 +29,126 @@ public class EmulatorDetector {
      * Detects if app is currenly running on emulator, or real device.
      * @return true for emulator, false for real devices
      */
-    public static boolean isEmulator() {
+    public static boolean deviceIsEmulator() {
+        int rating = 0;
 
-        if(rating < 0) { // rating is not calculated yet
-            int newRating = 0;
-
-            if(Build.PRODUCT.equals("sdk") ||
-                    Build.PRODUCT.equals("google_sdk") ||
-                    Build.PRODUCT.equals("sdk_x86") ||
-                    Build.PRODUCT.equals("vbox86p")) {
-                newRating ++;
-            }
-
-            if(Build.MANUFACTURER.equals("unknown") ||
-                    Build.MANUFACTURER.equals("Genymotion")) {
-                newRating ++;
-            }
-
-            if(Build.BRAND.equals("generic") ||
-                    Build.BRAND.equals("generic_x86")) {
-                newRating ++;
-            }
-
-            if(Build.DEVICE.equals("generic") ||
-                    Build.DEVICE.equals("generic_x86") ||
-                    Build.DEVICE.equals("vbox86p")) {
-                newRating ++;
-            }
-
-            if(Build.MODEL.equals("sdk") ||
-                    Build.MODEL.equals("google_sdk") ||
-                    Build.MODEL.equals("Android SDK built for x86")) {
-                newRating ++;
-            }
-
-            if(Build.HARDWARE.equals("goldfish") ||
-                    Build.HARDWARE.equals("vbox86")) {
-                newRating ++;
-            }
-
-            if(Build.FINGERPRINT.contains("generic/sdk/generic") ||
-                    Build.FINGERPRINT.contains("generic_x86/sdk_x86/generic_x86") ||
-                    Build.FINGERPRINT.contains("generic/google_sdk/generic") ||
-                    Build.FINGERPRINT.contains("generic/vbox86p/vbox86p")) {
-                newRating ++;
-            }
-
-            rating = newRating;
+        if (Build.PRODUCT.contains("sdk") ||
+                Build.PRODUCT.contains("Andy") ||
+                Build.PRODUCT.contains("ttVM_Hdragon") ||
+                Build.PRODUCT.contains("google_sdk") ||
+                Build.PRODUCT.contains("Droid4X") ||
+                Build.PRODUCT.contains("nox") ||
+                Build.PRODUCT.contains("sdk_x86") ||
+                Build.PRODUCT.contains("sdk_google") ||
+                Build.PRODUCT.contains("vbox86p")) {
+            rating++;
         }
 
-        return rating > 4;
+        if (Build.MANUFACTURER.equals("unknown") ||
+                Build.MANUFACTURER.equals("Genymotion") ||
+                Build.MANUFACTURER.contains("Andy") ||
+                Build.MANUFACTURER.contains("MIT") ||
+                Build.MANUFACTURER.contains("nox") ||
+                Build.MANUFACTURER.contains("TiantianVM")){
+            rating++;
+        }
+
+        if (Build.BRAND.equals("generic") ||
+                Build.BRAND.equals("generic_x86") ||
+                Build.BRAND.equals("TTVM") ||
+                Build.BRAND.contains("Andy")) {
+            rating++;
+        }
+
+        if (Build.DEVICE.contains("generic") ||
+                Build.DEVICE.contains("generic_x86") ||
+                Build.DEVICE.contains("Andy") ||
+                Build.DEVICE.contains("ttVM_Hdragon") ||
+                Build.DEVICE.contains("Droid4X") ||
+                Build.DEVICE.contains("nox") ||
+                Build.DEVICE.contains("generic_x86_64") ||
+                Build.DEVICE.contains("vbox86p")) {
+            rating++;
+        }
+
+        if (Build.MODEL.equals("sdk") ||
+                Build.MODEL.equals("google_sdk") ||
+                Build.MODEL.contains("Droid4X") ||
+                Build.MODEL.contains("TiantianVM") ||
+                Build.MODEL.contains("Andy") ||
+                Build.MODEL.equals("Android SDK built for x86_64") ||
+                Build.MODEL.equals("Android SDK built for x86")) {
+            rating++;
+        }
+
+        if (Build.HARDWARE.equals("goldfish") ||
+                Build.HARDWARE.equals("vbox86") ||
+                Build.HARDWARE.contains("nox") ||
+                Build.HARDWARE.contains("ttVM_x86")) {
+            rating++;
+        }
+
+        if (Build.FINGERPRINT.contains("generic/sdk/generic") ||
+                Build.FINGERPRINT.contains("generic_x86/sdk_x86/generic_x86") ||
+                Build.FINGERPRINT.contains("Andy") ||
+                Build.FINGERPRINT.contains("ttVM_Hdragon") ||
+                Build.FINGERPRINT.contains("generic_x86_64") ||
+                Build.FINGERPRINT.contains("generic/google_sdk/generic") ||
+                Build.FINGERPRINT.contains("vbox86p") ||
+                Build.FINGERPRINT.contains("generic/vbox86p/vbox86p")) {
+            rating++;
+        }
+
+        try {
+            String opengl = android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_RENDERER);
+            if (opengl != null){
+                if( opengl.contains("Bluestacks") || 
+                    opengl.contains("Translator")
+                )
+                rating += 10;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            File sharedFolder = new File(Environment
+                    .getExternalStorageDirectory().toString()
+                    + File.separatorChar
+                    + "windows"
+                    + File.separatorChar
+                    + "BstSharedFolder");
+
+            if (sharedFolder.exists()) {
+                rating += 10;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BluetoothAdapter m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            String m_bluetoothAdd = m_BluetoothAdapter.getAddress();
+            if (m_bluetoothAdd == null){
+                rating += 3;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String kernelVersion = System.getProperty("os.version");
+
+            if (kernelVersion.contains("")){
+                //3.10.30-android-x86+ for bluestacks
+                //didnt find a reliable use for this yet
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rating > 3;
     }
 
     /**
@@ -89,7 +162,11 @@ public class EmulatorDetector {
                "Build.DEVICE: " + Build.DEVICE + "\n" +
                "Build.MODEL: " + Build.MODEL + "\n" +
                "Build.HARDWARE: " + Build.HARDWARE + "\n" +
-               "Build.FINGERPRINT: " + Build.FINGERPRINT;
+               "Build.FINGERPRINT: " + Build.FINGERPRINT + "\n" + 
+               "GL_RENDERER: " +android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_RENDERER) + "\n" + 
+               "GL_VENDOR: " +android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_VENDOR) + "\n" + 
+               "GL_VERSION: " +android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_VERSION) + "\n" + 
+               "GL_EXTENSIONS: " +android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_EXTENSIONS) + "\n";
     }
 
     /**
